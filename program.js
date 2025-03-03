2f77559c77db5131cf12bca9d68009e7946e6612
natarodri <natharhernandez17@gmail.com>
2025-03-02 21:13:05 -0500

    v0.3

--- program.js
+++ program.js
@@ -1,7 +1,7 @@
 var map = L.map('map').setView([4.551923606760363, -74.10008874583308], 16);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 15,
+    maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
@@ -16,75 +16,79 @@ async function loadPolygon() {
     }
     ).addTo (map);
 }
+
 loadPolygon();
 
-let btnTrees = document.getElementById('btnTrees');
+let btnTrees= document.getElementById("btnTrees");
+
 btnTrees.addEventListener('click',
+    async ()=>{
+        let response= await fetch("arboles_san_martin.geojson");
+        let datos= await response.json();
 
-async function loadPolygon() {
-    let myData = await fetch ("arboles_san_martin.geojson");
-    let datos = await response.json();
-    L.geoJSON (datos,
-    {
-        pointToLayer: (feature, latlong)=>{
-            radius:5,
-            fillColor:'green',
-            wight:1,
-            opacity:1
-            fillOpacity:1.0
-
-        }
-    }
-    ).addTo (map);
-}
-)
-let btndistance = document.getElementById('btnTrees');
-btnTrees.addEventListener('click',
-
-async function loadPolygon() 
-{
-    let myData = await fetch ("arboles_san_martin.geojson");
-    let datos = await response.json();
-
-    let trees= datos.feature.map((myElement,index)=>({
-        id:index+1,
-        coordenates: myElement.geometry.coordenates
-    }));
-    console.log(trees);
-    let distances=[];
-    trees.array.forEach(tree1) => {
-        trees.forEach
-        (
-            (tree2)=>{
-                //calcular la distancias
-                let distance = turf.distance(
-                    turf.point(tree1.coordinates),
-                    turf.point(tree2.coordinates),
-                );
-                distances.push(
-                    [
-                        Árbol ${tree1.id},
-                        Árbol ${tree2.id},
-                    distance.toFixed(3)
-                    ]
-                )
-
+        L.geoJSON(
+            datos,
+            {
+                pointToLayer: (feature, latlong)=>{                    
+                    return L.circleMarker(latlong,{
+                        radius:5,
+                        fillColor:'green',
+                        weight:1,
+                        opacity:1,
+                        fillOpacity: 0.5,
+                    })
+                }
             }
-        )
-    })
-    generatePDF(distaces, trees.lenght);
-}
+        ).addTo(map);
+    }
 )
-function generatePDF(distances, totalTrees){
-    let{jsPDF}= window.jspdf;
-    let documentPDF= new jsPDF();
+let btnDistance = document.getElementById('btnDistance');
+btnDistance.addEventListener('click',
+    async ()=>{
+        let response= await fetch("arboles_san_martin.geojson");
+        let datos= await response.json();
+        let trees= datos.features.map((myElement, index)=>({
+            id: index+1,
+            coordinates: myElement.geometry.coordinates
+        }));   
 
-    documentPDF.text("REPORTE DE ARBOLES EN EL BARRIO SAN MARTIN",10,10)
-    documentPDF.text("EL BARRIO SAN MARTIN TIENE"+totalTrees+ "ARBOLES",20,30)
-    if(typeof documentPDF.autoTable !="function")
+        let distances=[];
+        trees.forEach( (treeA)=>{trees.forEach(
+
+            
+                (treeB)=>{
+                    if(treeA.id != treeB.id){
+                        let distance = turf.distance( 
+                            turf.point(treeA.coordinates),
+                            turf.point(treeB.coordinates),
+                        );
+                        distances.push(
+                            [
+                                `Árbol ${treeA.id}`,
+                                `Árbol ${treeB.id}`,
+                                distance.toFixed(3)                            
+                            ]
+                        )
+                }
+            }
+            )
+        }
+        )
+        generatePDF(distances, trees.lenght);
+    }
+)
+        
+function generatePDF(distances, totalTrees){
+    let { jsPDF } = window.jspdf;
+    let documentPDF= new jsPDF();   
+    
+    documentPDF.text("REPORTE DE ÁRBOLES EN EL BARRIO SAN MARTIN", 10,10);
+
     documentPDF.autoTable(
-        head:{["arbol1","Arbol 3", "distance"]},
-        body: distances
+        {
+            head: [['Árbol 1', 'Árbol 2', 'Distance']],
+            body: distances
+        }
     );
-    documentPDF.save("san_marti.pdf")
+    documentPDF.save("SAN MARTIN.pdf")
 }
